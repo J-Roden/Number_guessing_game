4b23603e6f52cc8a1426e115f645b0fb1dcb4059
James Roden <roden7.3power@gmail.com>
2023-02-15 22:02:26 -0600

    added .gitignore

--- great_number_game/settings.py
+++ great_number_game/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qc&#)ocon%+zfmlu=i%um7ty7-o5ylrvl$k3326^gyahqtclc7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
